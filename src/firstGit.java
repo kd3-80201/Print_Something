f08f8cf280692d4d5d23da8b4b2fa08f5e848c40
Afnan Baig <mirzaafnanbaig98@gmail.com>
2022-09-02 20:05:23 +0530

    added new one texts

--- src/firstGit.java
+++ src/firstGit.java
@@ -1,5 +1,6 @@
 public class firstGit {
     public static void main(String[] args) {
         System.out.println("Keep trying and be consistent");
+        System.out.println("added extra thing");
     }
 }
